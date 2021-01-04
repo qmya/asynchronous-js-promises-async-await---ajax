c2ec4f86301a6e55fba95b382b6a43446ac25167
qmya <qandeelgul90@gmail.com>
2021-01-04 10:31:41 -0500

    get country element and insert the HTML in the DOM

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ request.addEventListener('load', function () {
   //to change the json data from string to parse
   const [data] = JSON.parse(this.responseText); //this is request
   console.log(data);
+  const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag} "/>
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row">
+                    <span>👫</span>${(data.population / 1000000).toFixed(1)}
+                </p>
+                <p class="country__row">
+                    <span>🗣️</span>${data.languages[1].name} and ${
+    data.languages[0].name
+  }
+                </p>
+                <p class="country__row">
+                    <span>💰</span>${data.currencies[0].name}
+                </p>
+            </div>
+    </article>;
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
