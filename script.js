c9bc30e86b3d64e5170ee0a8e82244ab34fdb4fd
qmya <qandeelgul90@gmail.com>
2021-01-04 10:08:17 -0500

    Requested a data using XML

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const countriesContainer = document.querySelector('.countries');
 //XML HTTP request function:
 const request = new XMLHttpRequest(); //first step
 //We are doing this becoz how AJAX calls used to handled with events & call backs ftns
+request.open('GET', 'https://restcountries.eu/rest/v2/name/pakistan'); //in this step we want to request the URL and the type of request
+request.send(); //this will send the request to the URl
+console.log(request.responseText);
+request.addEventListener('load', function () {
+  //to change the json data from string to parse
+  const [data] = JSON.parse(this.responseText); //this is request
+  console.log(data);
+});
