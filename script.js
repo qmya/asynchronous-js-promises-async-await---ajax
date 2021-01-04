0a4c712c4d2f72670377d928a1f218daa534c434
qmya <qandeelgul90@gmail.com>
2021-01-04 09:43:09 -0500

    Using old school way, XML HTTP request function 'becoz how AJAX calls used to handled with events & call backs ftns '

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//in AJAX call we use lots of ways to request data
+//but
+//Here we will use old way :
+//XML HTTP request function:
+const request = new XMLHttpRequest(); //first step
+//We are doing this becoz how AJAX calls used to handled with events & call backs ftns
